7688264a74aeb1ba851d169fe42ebd30866ad94f
mykirk98 <mykirk98@gmail.com>
2024-02-29 10:07:51 +0900

    commit 2024/02/29

--- Q2501.py
+++ Q2501.py
@@ -20,24 +20,17 @@
 
 # import math
 
-# N, K = map(int, input().split())
+N, K = map(int, input().split())
 
-# divisor = []
+divisor = []
 # for i in range(1, int(math.sqrt(N) + 1)):
 #     if N % i == 0:
 #         divisor.append(i)
 #         divisor.append(N // i)
-
 # divisor = list(set(divisor))
-
-# print(0 if K > len(divisor) else divisor[K-1])
-
-
-N, K = map(int, input().split())
-divisor = []
 
 for i in range(1, N + 1):
     if N % i == 0:
         divisor.append(i)
-print(divisor)
+        
 print(0 if K > len(divisor) else divisor[K-1])
